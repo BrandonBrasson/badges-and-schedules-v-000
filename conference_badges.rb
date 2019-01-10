43ab58946de87cd8c4bbaae4637feb34a6d1b06d
Brandon Brasson <rocksong622@gmail.com>
2019-01-10 17:12:33 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,30 @@
-# Write your code here.
+def badge_maker(n)
+  return "Hello, my name is #{n}."
+end
+
+def creator(n)
+  # returns an array of badge messages, using badge_maker
+  badge_messages = []
+  n.each do |speaker|
+    message = badge_maker(n)
+    badge_messages << message
+  end
+  badge_messages
+end
+
+def rooms(n)
+  room_number = 1
+  room_messages = []
+  n.each do |speaker|
+    room_messages << "Hello, #{n}! You'll be assigned to room #{room_number}!"
+    room_number += 1
+  end
+  room_messages
+end
+
+def print(n)
+  badge_messages = batch_badge_creator(speakers)
+  badge_messages.each do |message|
+    puts message
+  end
+end
