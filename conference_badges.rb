2bd0109b4dbf2640b7cdee0831cb6e8577d6a341
Brandon Brasson <rocksong622@gmail.com>
2019-01-10 17:51:17 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -23,7 +23,7 @@ def assign_rooms(speakers)
   room_messages
 end
 
-def printer(n)
+def printer(speakers)
   # outputs the results of batch_badge_creator, and assign_rooms
   badge_messages = batch_badge_creator(speakers)
   badge_messages.each do |message|
